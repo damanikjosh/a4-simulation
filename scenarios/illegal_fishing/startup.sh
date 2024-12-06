72344c5bc866951aca57113bc107799cbab9be9f
Joshua J. Damanik <joshuajdmk@gmail.com>
2024-12-07 05:18:43 +0900

    Fix wam-v control and add vesel-e vehicle

--- scenarios/illegal_fishing/startup.sh
+++ scenarios/illegal_fishing/startup.sh
@@ -11,26 +11,16 @@ echo "Waiting for Gazebo..." && sleep 5
 # Tutorial: https://docs.px4.io/main/en/sim_gazebo_gz/multi_vehicle_simulation.html#multiple-vehicles-with-ros-2-and-gazebo
 
 # Spawn quadrotors
-# sleep 5 && echo "Spawning..."
-# PX4_GZ_MODEL_POSE=0,10,1.3,0,0,0 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500_depth /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 1 > /dev/null 2>&1 &
-# sleep 5 && echo "Spawning..."
-# PX4_GZ_MODEL_POSE=2,10,1.3,0,0,0 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500_depth /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 2 > /dev/null 2>&1 &
+sleep 5 && echo "Spawning..."
+PX4_GZ_MODEL_POSE=-500,-200,2,0,0,0 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500_depth /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 1 > /dev/null 2>&1 &
+sleep 5 && echo "Spawning..."
+PX4_GZ_MODEL_POSE=-500,-202,2,0,0,0 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500_depth /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 2 > /dev/null 2>&1 &
 
 # Spawn rover
 sleep 5 && echo "Spawning..."
 PX4_GZ_MODEL_POSE=-600,-180,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 3 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,-803,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 3 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,635,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 3 > /dev/null 2>&1 &
-
 sleep 5 && echo "Spawning..."
-PX4_GZ_MODEL_POSE=-600,-170,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 4 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,-793,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 4 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,645,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 4 > /dev/null 2>&1 &
-
-sleep 5 && echo "Spawning..."
-PX4_GZ_MODEL_POSE=-600,-160,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 5 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,-783,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 5 > /dev/null 2>&1 &
-# PX4_GZ_MODEL_POSE=-600,655,0,0,0,3.14 PX4_SYS_AUTOSTART=22000 PX4_SIM_MODEL=wam-v /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 5 > /dev/null 2>&1 &
+PX4_GZ_MODEL_POSE=-900,-180,0,0,0,3.14 PX4_SYS_AUTOSTART=22001 PX4_SIM_MODEL=vessel-e /root/PX4-Autopilot/build/px4_sitl_default/bin/px4 -i 4 > /dev/null 2>&1 &
 
 # Run the script here
 # python3 ${SCRIPT_DIR}/mission_single.py
